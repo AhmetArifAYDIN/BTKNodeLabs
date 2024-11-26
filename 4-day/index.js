2598c7993346bd7f824be98b13df5fadaccf765b
Ahmet Arif AYDIN <ahmetarifaydina@gmail.com>
2024-11-26 15:06:03 +0300

    Add files via upload

---
+++ 4-day/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+const ogrenciRoute = require("./routes/ogrenci");
+
+app.use("/ogrenci", ogrenciRoute);
+const MONGO_URI = "";
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+const OgretmenSchema = new mongoose.Schema({
+  adi: { type: String },
+  alani: { type: String },
+  tel_no: { type: String },
+});
+
+const VeliSchema = new mongoose.Schema({
+  adi: { type: String },
+  ogrenci_adi: { type: String },
+});
+
+const Ogretmen = mongoose.model("Ogretmen", OgretmenSchema);
+const Veli = mongoose.model("Veli", VeliSchema);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Sunucu şu portta çalışıyor : ${PORT}`);
+});
